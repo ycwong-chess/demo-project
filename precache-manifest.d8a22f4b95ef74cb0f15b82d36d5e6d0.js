973f8064026f51d2fee55ec946bf4da25c4e8c7f
ycwong-chess <ycwong429@gmail.com>
2020-08-12 13:40:59 +0800

    Updates

---
+++ precache-manifest.d8a22f4b95ef74cb0f15b82d36d5e6d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093d60669c76fca80532bf54096ae664",
+    "url": "/demo-project/index.html"
+  },
+  {
+    "revision": "a3f8a15588c7264ae01c",
+    "url": "/demo-project/static/css/main.b3b62682.chunk.css"
+  },
+  {
+    "revision": "ff76849d54511632be4a",
+    "url": "/demo-project/static/js/2.1686fb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-project/static/js/2.1686fb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f8a15588c7264ae01c",
+    "url": "/demo-project/static/js/main.5db6d681.chunk.js"
+  },
+  {
+    "revision": "ad2a5bb4391bd0da330e",
+    "url": "/demo-project/static/js/runtime-main.a9533d1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a5efe2279432d8e1b9318053345e41a6",
+    "url": "/demo-project/static/media/spirited-away.a5efe227.jpg"
+  }
+]);
