11b67c260d3c7e7a18907b0490da7ed54fc47245
ycwong-chess <ycwong429@gmail.com>
2020-08-13 16:41:47 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Quiz from './Quiz';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
